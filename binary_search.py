1bcaf6789d7d487c093e2d2d36264e6170b5861a
Anna Hoffman <142194685+Anna-M-Hoffman@users.noreply.github.com>
2025-10-17 19:28:42 -0700

    Add binary search function in binary_search.py
    Implement binary search algorithm to find target index.

---
+++ binary_search.py
@@ -0,0 +1,20 @@
+# Leetcode BINARY SEARCH
+# Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+# Write an algorithm with O(log n) runtime complexity.
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = l + (r - l) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target: 
+                # Search right side
+                l = mid + 1
+            else:
+                # Search left side
+                r = mid - 1
+
+        return -1 # Target not found
